abcbfb717557b2f626d43b94d54c25b274da5b4a
animetosho <animetosho@users.noreply.github.com>
2015-09-19 14:55:32 +1000

    Slightly optimise JIT code generation routines

--- gf-complete/x86_jit.c
+++ gf-complete/x86_jit.c
@@ -15,46 +15,58 @@
 	#define RXX_PREFIX
 #endif
 
-void _jit__enc_offset(jit_t* jit, int32_t offs) {
-	if(!offs) return;
-	if(offs <= 127 && offs >= -128) {
-		jit->code[jit->pos -1] |= 0x40;
-		jit->code[jit->pos++] = (int8_t)offs;
-	} else {
-		jit->code[jit->pos -1] |= 0x80;
-		*(int32_t*)(jit->code + jit->pos) = offs;
+void _jit_xorps_m(jit_t* jit, uint8_t xreg, uint8_t mreg, int32_t offs) {
+	if((offs+128) & ~0xFF) {
+		*(int32_t*)(jit->code + jit->pos) = 0x80570F | (xreg <<19) | (mreg <<16);
+		*(int32_t*)(jit->code + jit->pos +3) = offs;
+		jit->pos += 7;
+	} else if(offs) {
+		*(int32_t*)(jit->code + jit->pos) = 0x40570F | (xreg <<19) | (mreg <<16) | (offs <<24);
 		jit->pos += 4;
+	} else {
+		/* can overflow, but we don't care */
+		*(int32_t*)(jit->code + jit->pos) = 0x570F | (xreg <<19) | (mreg <<16);
+		jit->pos += 3;
 	}
 }
-
-void _jit_xorps_m(jit_t* jit, uint8_t xreg, uint8_t mreg, int32_t offs) {
-	jit->code[jit->pos++] = 0x0F;
-	jit->code[jit->pos++] = 0x57;
-	jit->code[jit->pos++] = (xreg << 3) | mreg;
-	_jit__enc_offset(jit, offs);
-}
 void _jit_xorps_r(jit_t* jit, uint8_t xreg2, uint8_t xreg1) {
-	jit->code[jit->pos++] = 0x0F;
-	jit->code[jit->pos++] = 0x57;
-	jit->code[jit->pos++] = 0xC0 | (xreg2 << 3) | xreg1;
+	/* can overflow, but we don't care */
+	*(int32_t*)(jit->code + jit->pos) = 0xC0570F | (xreg2 <<19) | (xreg1 <<16);
+	jit->pos += 3;
 }
 
 void _jit_movaps(jit_t* jit, uint8_t xreg, uint8_t xreg2) {
-	jit->code[jit->pos++] = 0x0F;
-	jit->code[jit->pos++] = 0x28;
-	jit->code[jit->pos++] = 0xC0 | (xreg << 3) | xreg2;
+	/* can overflow, but we don't care */
+	*(int32_t*)(jit->code + jit->pos) = 0xC0280F | (xreg <<19) | (xreg2 <<16);
+	jit->pos += 3;
 }
 void _jit_movaps_load(jit_t* jit, uint8_t xreg, uint8_t mreg, uint8_t offs) {
-	jit->code[jit->pos++] = 0x0F;
-	jit->code[jit->pos++] = 0x28;
-	jit->code[jit->pos++] = (xreg << 3) | mreg;
-	_jit__enc_offset(jit, offs);
+	if((offs+128) & ~0xFF) {
+		*(int32_t*)(jit->code + jit->pos) = 0x80280F | (xreg <<19) | (mreg <<16);
+		*(int32_t*)(jit->code + jit->pos +3) = offs;
+		jit->pos += 7;
+	} else if(offs) {
+		*(int32_t*)(jit->code + jit->pos) = 0x40280F | (xreg <<19) | (mreg <<16) | (offs <<24);
+		jit->pos += 4;
+	} else {
+		/* can overflow, but we don't care */
+		*(int32_t*)(jit->code + jit->pos) = 0x280F | (xreg <<19) | (mreg <<16);
+		jit->pos += 3;
+	}
 }
 void _jit_movaps_store(jit_t* jit, uint8_t mreg, uint8_t offs, uint8_t xreg) {
-	jit->code[jit->pos++] = 0x0F;
-	jit->code[jit->pos++] = 0x29;
-	jit->code[jit->pos++] = (xreg << 3) | mreg;
-	_jit__enc_offset(jit, offs);
+	if((offs+128) & ~0xFF) {
+		*(int32_t*)(jit->code + jit->pos) = 0x80290F | (xreg <<19) | (mreg <<16);
+		*(int32_t*)(jit->code + jit->pos +3) = offs;
+		jit->pos += 7;
+	} else if(offs) {
+		*(int32_t*)(jit->code + jit->pos) = 0x40290F | (xreg <<19) | (mreg <<16) | (offs <<24);
+		jit->pos += 4;
+	} else {
+		/* can overflow, but we don't care */
+		*(int32_t*)(jit->code + jit->pos) = 0x290F | (xreg <<19) | (mreg <<16);
+		jit->pos += 3;
+	}
 }
 
 void _jit_push(jit_t* jit, uint8_t reg) {
@@ -65,50 +77,56 @@ void _jit_pop(jit_t* jit, uint8_t reg) {
 }
 void _jit_jmp(jit_t* jit, int32_t addr) {
 	int32_t target = addr - jit->pos -2;
-	if(target <= 127 && target >= -128) {
-		jit->code[jit->pos++] = 0xEB;
-		jit->code[jit->pos++] = (int8_t)target;
-	} else {
+	if((target+128) & ~0xFF) {
 		jit->code[jit->pos++] = 0xE9;
 		*(int32_t*)(jit->code + jit->pos) = target -3;
 		jit->pos += 4;
+	} else {
+		*(int16_t*)(jit->code + jit->pos) = 0xEB | ((int8_t)target << 8);
+		jit->pos += 2;
 	}
 }
 void _jit_jcc(jit_t* jit, char op, int32_t addr) {
 	int32_t target = addr - jit->pos -2;
-	if(target <= 127 && target >= -128) {
-		jit->code[jit->pos++] = 0x70 | op;
-		jit->code[jit->pos++] = (int8_t)target;
-	} else {
+	if((target+128) & ~0xFF) {
 		jit->code[jit->pos++] = 0x0F;
 		jit->code[jit->pos++] = 0x80 | op;
 		*(int32_t*)(jit->code + jit->pos) = target -4;
 		jit->pos += 4;
+	} else {
+		*(int16_t*)(jit->code + jit->pos) = 0x70 | op | ((int8_t)target << 8);
+		jit->pos += 2;
 	}
 }
 void _jit_cmp_r(jit_t* jit, uint8_t reg, uint8_t reg2) {
 	RXX_PREFIX
-	jit->code[jit->pos++] = 0x39;
-	jit->code[jit->pos++] = 0xC0 | (reg2 << 3) | reg;
+	*(int16_t*)(jit->code + jit->pos) = 0xC039 | (reg2 << 11) | (reg << 8);
+	jit->pos += 2;
 }
 void _jit_add_i(jit_t* jit, uint8_t reg, int32_t val) {
 	RXX_PREFIX
-	jit->code[jit->pos++] = 0x81;
-	jit->code[jit->pos++] = 0xC0 | reg;
+	*(int16_t*)(jit->code + jit->pos) = 0xC081 | (reg << 8);
+	jit->pos += 2;
 	*(int32_t*)(jit->code + jit->pos) = val;
 	jit->pos += 4;
 }
-void _jit_mov_i(jit_t* jit, uint8_t reg, long val) {
+void _jit_sub_i(jit_t* jit, uint8_t reg, int32_t val) {
+	RXX_PREFIX
+	*(int16_t*)(jit->code + jit->pos) = 0xC083 | (reg << 8);
+	jit->pos += 2;
+	*(int32_t*)(jit->code + jit->pos) = val;
+	jit->pos += 4;
+}
+void _jit_mov_i(jit_t* jit, uint8_t reg, intptr_t val) {
 #ifdef AMD64
 	if(val > 0x3fffffff || val < 0x40000000) {
-		jit->code[jit->pos++] = 0x48; 
-		jit->code[jit->pos++] = 0xB8 | reg;
+		*(int16_t*)(jit->code + jit->pos) = 0xB848 | (reg << 8);
+		jit->pos += 2;
 		*(int64_t*)(jit->code + jit->pos) = val;
 		jit->pos += 8;
 	} else {
-		jit->code[jit->pos++] = 0x48; 
-		jit->code[jit->pos++] = 0xC7; 
-		jit->code[jit->pos++] = 0xC0 | reg;
+		*(int32_t*)(jit->code + jit->pos) = 0xC0C748 | (reg << 16);
+		jit->pos += 3;
 		*(int32_t*)(jit->code + jit->pos) = val;
 		jit->pos += 4;
 	}
