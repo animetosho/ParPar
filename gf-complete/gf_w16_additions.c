2c27a13c90b293048189ab7b3c4d4ef36d3b0b18
animetosho <animetosho@users.noreply.github.com>
2017-11-22 21:30:05 +1000

    Add AVX512 version of the AFFINE algorithm

--- gf-complete/gf_w16_additions.c
+++ gf-complete/gf_w16_additions.c
@@ -2058,8 +2058,129 @@ static void gf_w16_affine_multiply_region(gf_t *gf, void *src, void *dest, gf_va
       
     }
   }
-  //_MM_END
 }
+
+#ifdef INTEL_AVX512BW
+static void gf_w16_affine512_multiply_region(gf_t *gf, void *src, void *dest, gf_val_32_t val, int bytes, int xor)
+{
+  FAST_U32 i, bit;
+  long inBit;
+  __m512i* sW, * dW, * topW;
+  __m512i ta, tb, tpl, tph;
+  __m128i depmask1, depmask2, polymask1, polymask2, addvals1, addvals2;
+  gf_region_data rd;
+  gf_internal_t *h = (gf_internal_t *) gf->scratch;
+
+  if (val == 0) { gf_multby_zero(dest, bytes, xor); return; }
+  if (val == 1) { gf_multby_one(src, dest, bytes, xor); return; }
+
+  h = (gf_internal_t *) gf->scratch;
+  gf_w16_log_region_alignment(&rd, gf, src, dest, bytes, val, xor, 64, 128);
+  
+  /* calculate dependent bits */
+  addvals1 = _mm_set_epi16(0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80);
+  addvals2 = _mm_set_epi16(0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000);
+    
+  /* duplicate each bit in the polynomial 16 times */
+  polymask2 = _mm_set1_epi16(h->prim_poly & 0xFFFF); /* chop off top bit, although not really necessary */
+  polymask1 = _mm_and_si128(polymask2, addvals2);
+  polymask2 = _mm_and_si128(polymask2, addvals1);
+  polymask1 = _mm_cmpeq_epi16(_mm_setzero_si128(), polymask1);
+  polymask2 = _mm_cmpeq_epi16(_mm_setzero_si128(), polymask2);
+    
+  if(val & (1<<15)) {
+    /* XOR */
+    depmask1 = addvals1;
+    depmask2 = addvals2;
+  } else {
+    depmask1 = _mm_setzero_si128();
+    depmask2 = _mm_setzero_si128();
+  }
+  for(i=(1<<14); i; i>>=1) {
+    /* rotate */
+    __m128i last = _mm_shuffle_epi8(depmask1, _mm_set_epi8(1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0));
+    depmask1 = _mm_alignr_epi8(depmask2, depmask1, 2);
+    depmask2 = _mm_srli_si128(depmask2, 2);
+    
+    /* XOR poly */
+    depmask1 = _mm_xor_si128(depmask1, _mm_andnot_si128(polymask1, last));
+    depmask2 = _mm_xor_si128(depmask2, _mm_andnot_si128(polymask2, last));
+    
+    if(val & i) {
+      /* XOR */
+      depmask1 = _mm_xor_si128(depmask1, addvals1);
+      depmask2 = _mm_xor_si128(depmask2, addvals2);
+    }
+  }
+    
+  __m512i mat_ll, mat_lh, mat_hl, mat_hh;
+  __m128i high_half = _mm_set_epi8(
+    14,12,10,8,6,4,2,0, 14,12,10,8,6,4,2,0
+  ), low_half = _mm_set_epi8(
+    15,13,11,9,7,5,3,1, 15,13,11,9,7,5,3,1
+  );
+  mat_lh = _mm512_broadcast_i32x4(_mm_shuffle_epi8(depmask2, high_half));
+  mat_ll = _mm512_broadcast_i32x4(_mm_shuffle_epi8(depmask2, low_half));
+  mat_hh = _mm512_broadcast_i32x4(_mm_shuffle_epi8(depmask1, high_half));
+  mat_hl = _mm512_broadcast_i32x4(_mm_shuffle_epi8(depmask1, low_half));
+  
+  
+  sW = (__m512i *) rd.s_start;
+  dW = (__m512i *) rd.d_start;
+  topW = (__m512i *) rd.d_top;  
+  
+  if (xor) {
+    while (dW != topW) {
+
+      ta = _mm512_load_si512(sW);
+      tb = _mm512_load_si512(sW+1);
+
+      tpl = _mm512_ternarylogic_epi32(
+        _mm512_gf2p8affine_epi64_epi8(ta, mat_lh, 0),
+        _mm512_gf2p8affine_epi64_epi8(tb, mat_ll, 0),
+        _mm512_load_si512(dW+1),
+        0x96
+      );
+      tph = _mm512_ternarylogic_epi32(
+        _mm512_gf2p8affine_epi64_epi8(ta, mat_hh, 0),
+        _mm512_gf2p8affine_epi64_epi8(tb, mat_hl, 0),
+        _mm512_load_si512(dW),
+        0x96
+      );
+
+      _mm512_store_si512 (dW, tph);
+      _mm512_store_si512 (dW+1, tpl);
+
+      dW += 2;
+      sW += 2;
+    }
+  } else {
+    while (dW != topW) {
+
+      ta = _mm512_load_si512(sW);
+      tb = _mm512_load_si512(sW+1);
+
+      tpl = _mm512_xor_si512(
+        _mm512_gf2p8affine_epi64_epi8(ta, mat_lh, 0),
+        _mm512_gf2p8affine_epi64_epi8(tb, mat_ll, 0)
+      );
+      tph = _mm512_xor_si512(
+        _mm512_gf2p8affine_epi64_epi8(ta, mat_hh, 0),
+        _mm512_gf2p8affine_epi64_epi8(tb, mat_hl, 0)
+      );
+
+      _mm512_store_si512 (dW, tph);
+      _mm512_store_si512 (dW+1, tpl);
+
+      dW += 2;
+      sW += 2;
+      
+    }
+  }
+  _mm256_zeroupper();
+}
+#endif /* INTEL_AVX512BW */
+
 #endif /* INTEL_GFNI */
 
 
