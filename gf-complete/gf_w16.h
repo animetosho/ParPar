8d67bbf9e41b5681547f0a9585043863781b1cde
animetosho <animetosho@users.noreply.github.com>
2018-03-10 21:37:38 +1000

    Tweak gf_w16_log_region_alignment to favour aligned input

--- gf-complete/gf_w16.h
+++ gf-complete/gf_w16.h
@@ -54,52 +54,76 @@ static inline void gf_w16_log_region_alignment(gf_region_data *rd,
   int align,
   int walign)
 {
-  uintptr_t uls;
-  struct gf_w16_logtable_data *ltd = (struct gf_w16_logtable_data *) ((gf_internal_t *) gf->scratch)->private;
-  int log_val = ltd->log_tbl[val];
-  uint16_t *sEnd = ((uint16_t*)src) + (bytes>>1);
+	uintptr_t uls = ((uintptr_t) src) & (align-1);
+	struct gf_w16_logtable_data *ltd;
+	int log_val;
   
 /* never used, so don't bother setting them
-  rd->gf = gf;
-  rd->src = src;
-  rd->dest = dest;
-  rd->bytes = bytes;
-  rd->val = val;
-  rd->xor = xor;
+	rd->gf = gf;
+	rd->src = src;
+	rd->dest = dest;
+	rd->bytes = bytes;
+	rd->val = val;
+	rd->xor = xor;
 */
 
-  uls = ((uintptr_t) src) & (align-1);
-
-  if (uls != (((uintptr_t) dest) & (align-1)))
-    assert(0);
-  if ((bytes & 1) != 0)
-    assert(0);
-
-  if (uls != 0) uls = (align-uls);
-  rd->s_start = (uint8_t *)src + uls;
-  rd->d_start = (uint8_t *)dest + uls;
-  bytes -= uls;
-  bytes -= (bytes & (walign-1));
-  rd->s_top = (uint8_t *)rd->s_start + bytes;
-  rd->d_top = (uint8_t *)rd->d_start + bytes;
-
-  /* slow multiply for init/end area */
-  #define MUL_LOOP(op, src, dest, srcto) { \
-    uint16_t *s16 = (uint16_t *)src, *d16 = (uint16_t *)dest; \
-    while (s16 < (uint16_t *)(srcto)) { \
-      *d16 op (*s16 == 0) ? 0 : GF_ANTILOG((int) ltd->log_tbl[*s16] + log_val); \
-      s16++; \
-      d16++; \
-    } \
-  }
-  if (xor) {
-    MUL_LOOP(^=, src, dest, rd->s_start)
-    MUL_LOOP(^=, rd->s_top, rd->d_top, sEnd)
-  } else {
-    MUL_LOOP(=, src, dest, rd->s_start)
-    MUL_LOOP(=, rd->s_top, rd->d_top, sEnd)
-  }
-  #undef MUL_LOOP
+	if (uls != (((uintptr_t) dest) & (align-1)))
+		assert(0);
+	if ((bytes & 1) != 0)
+		assert(0);
+	
+	/* slow multiply for init/end area */
+	#define MUL_LOOP(op, src, dest, srcto) { \
+		uint16_t *s16 = (uint16_t *)src, *d16 = (uint16_t *)dest; \
+		while (s16 < (uint16_t *)(srcto)) { \
+			*d16 op (*s16 == 0) ? 0 : GF_ANTILOG((int) ltd->log_tbl[*s16] + log_val); \
+			s16++; \
+			d16++; \
+		} \
+	}
+	
+	if(uls != 0) {
+		int proc_bytes = bytes;
+		uls = (align-uls);
+		rd->s_start = (uint8_t *)src + uls;
+		rd->d_start = (uint8_t *)dest + uls;
+		proc_bytes -= uls;
+		proc_bytes -= (proc_bytes & (walign-1));
+		rd->s_top = (uint8_t *)rd->s_start + proc_bytes;
+		rd->d_top = (uint8_t *)rd->d_start + proc_bytes;
+		
+		ltd = (struct gf_w16_logtable_data *) ((gf_internal_t *) gf->scratch)->private;
+		log_val = ltd->log_tbl[val];
+		if (xor) {
+			MUL_LOOP(^=, src, dest, rd->s_start)
+			MUL_LOOP(^=, rd->s_top, rd->d_top, ((uint16_t*)src) + (bytes>>1))
+		} else {
+			MUL_LOOP(=, src, dest, rd->s_start)
+			MUL_LOOP(=, rd->s_top, rd->d_top, ((uint16_t*)src) + (bytes>>1))
+		}
+	} else {
+		unsigned int ule = bytes & (walign-1);
+		rd->s_start = (uint8_t *)src;
+		rd->d_start = (uint8_t *)dest;
+		if(ule != 0) {
+			int proc_bytes = bytes - ule;
+			rd->s_top = (uint8_t *)rd->s_start + proc_bytes;
+			rd->d_top = (uint8_t *)rd->d_start + proc_bytes;
+			
+			ltd = (struct gf_w16_logtable_data *) ((gf_internal_t *) gf->scratch)->private;
+			log_val = ltd->log_tbl[val];
+			if (xor) {
+				MUL_LOOP(^=, rd->s_top, rd->d_top, ((uint16_t*)src) + (bytes>>1))
+			} else {
+				MUL_LOOP(=, rd->s_top, rd->d_top, ((uint16_t*)src) + (bytes>>1))
+			}
+		} else {
+			rd->s_top = (uint8_t *)rd->s_start + bytes;
+			rd->d_top = (uint8_t *)rd->d_start + bytes;
+		}
+	}
+	
+	#undef MUL_LOOP
 }
 
 
