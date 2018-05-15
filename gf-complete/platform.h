28a96fc1424fd4bbd5819cbe689a674ad9ff4a08
animetosho <animetosho@users.noreply.github.com>
2018-05-15 11:52:04 +1000

    Minor MSVC ARM tweaks Still probably doesn't work, but maybe we're closer

--- gf-complete/platform.h
+++ gf-complete/platform.h
@@ -11,98 +11,111 @@
 	#define AMD64 1
 #endif
 
+# ifdef _M_ARM64
+	#define ARM_NEON 1
+	#define ARCH_AARCH64 1
+# else
+#  ifdef __ARM_NEON
+	#define ARM_NEON 1
+#  endif
+#  if defined(__ARM_ARCH) && __ARM_ARCH >= 8
+	#define ARCH_AARCH64 1
+#  endif
+# endif
 
 #ifdef _MSC_VER
 
-#if (defined(_M_IX86_FP) && _M_IX86_FP == 2) || defined(_M_X64)
+# if (defined(_M_IX86_FP) && _M_IX86_FP == 2) || defined(_M_X64)
 	#define INTEL_SSE2 1
 	#define INTEL_SSSE3 1
 	#if _MSC_VER >= 1600
 		#define INTEL_SSE4_PCLMUL 1
 	#endif
-#endif
-#if defined(__AVX2__) || (_MSC_VER >= 1800 && defined(INTEL_SSE2))
+# endif
+# if defined(__AVX2__) || (_MSC_VER >= 1800 && defined(INTEL_SSE2))
 	#define INTEL_AVX2 1
-#endif
-#if _MSC_VER >= 1911 && defined(INTEL_AVX2)
+# endif
+# if _MSC_VER >= 1911 && defined(INTEL_AVX2)
 	#define INTEL_AVX512BW 1
-#endif
-#if _MSC_VER >= 1912 && defined(INTEL_AVX512BW)
+# endif
+# if _MSC_VER >= 1912 && defined(INTEL_AVX512BW)
 	#define INTEL_AVX512VL 1
-#endif
+# endif
 
-#ifdef _WIN64
+# ifdef _WIN64
 typedef unsigned __int64 FAST_U8;
 typedef unsigned __int64 FAST_U16;
 typedef unsigned __int64 FAST_U32;
-#define FAST_U8_SIZE 8
-#define FAST_U16_SIZE 8
-#define FAST_U32_SIZE 8
-#else
+#  define FAST_U8_SIZE 8
+#  define FAST_U16_SIZE 8
+#  define FAST_U32_SIZE 8
+# else
 typedef unsigned __int32 FAST_U8;
 typedef unsigned __int32 FAST_U16;
 typedef unsigned __int32 FAST_U32;
-#define FAST_U8_SIZE 4
-#define FAST_U16_SIZE 4
-#define FAST_U32_SIZE 4
-#endif
+#  define FAST_U8_SIZE 4
+#  define FAST_U16_SIZE 4
+#  define FAST_U32_SIZE 4
+# endif
 
 #else
 
-#ifdef __SSE2__
+# ifdef __SSE2__
 	#define INTEL_SSE2 1
-#endif
-#ifdef __SSSE3__
+# endif
+# ifdef __SSSE3__
 	#define INTEL_SSSE3 1
-#endif
-#ifdef __PCLMUL__
+# endif
+# ifdef __PCLMUL__
 	#define INTEL_SSE4_PCLMUL 1
-#endif
-#ifdef __AVX2__
+# endif
+# ifdef __AVX2__
 	#define INTEL_AVX2 1
-#endif
-#ifdef __AVX512BW__
+# endif
+# ifdef __AVX512BW__
 	#define INTEL_AVX512BW 1
-#endif
+# endif
 
-/*#define ARCH_AARCH64 1*/
-/*#define ARM_NEON 1*/
 
-#if __WORDSIZE == 64
+# if defined(__WORDSIZE)
+#  if __WORDSIZE == 64
 typedef uint64_t FAST_U8;
 typedef uint64_t FAST_U16;
 typedef uint64_t FAST_U32;
-#elif __WORDSIZE == 32
+#  elif __WORDSIZE == 32
 typedef uint32_t FAST_U8;
 typedef uint32_t FAST_U16;
 typedef uint32_t FAST_U32;
-#else
+#  else
 typedef uint_fast8_t FAST_U8;
 typedef uint_fast16_t FAST_U16;
 typedef uint_fast32_t FAST_U32;
-#endif
+#  endif
 /* not ideal if wordsize is not 32/64... */
-#define FAST_U8_SIZE (__WORDSIZE / 8)
-#define FAST_U16_SIZE (__WORDSIZE / 8)
-#define FAST_U32_SIZE (__WORDSIZE / 8)
+#  define FAST_U8_SIZE (__WORDSIZE / 8)
+#  define FAST_U16_SIZE (__WORDSIZE / 8)
+#  define FAST_U32_SIZE (__WORDSIZE / 8)
+# elif defined(ARCH_AARCH64)
+typedef uint64_t FAST_U8;
+typedef uint64_t FAST_U16;
+typedef uint64_t FAST_U32;
+#  define FAST_U8_SIZE 8
+#  define FAST_U16_SIZE 8
+#  define FAST_U32_SIZE 8
+# else
+typedef uint_fast8_t FAST_U8;
+typedef uint_fast16_t FAST_U16;
+typedef uint_fast32_t FAST_U32;
+#  define FAST_U8_SIZE 1
+#  define FAST_U16_SIZE 2
+#  define FAST_U32_SIZE 4
+# endif
 
 
 #endif /* _MSC_VER */
 
 #ifdef __GFNI__
 	#define INTEL_GFNI
-#endif
-
-#ifdef _M_ARM64
-	#define ARM_NEON 1
-	#define ARCH_AARCH64 1
-#else
-# ifdef __ARM_NEON
-	#define ARM_NEON 1
-# endif
-# if defined(__ARM_ARCH) && __ARM_ARCH >= 8
-	#define ARCH_AARCH64 1
-# endif
 #endif
 
 #endif /* GF_COMPLETE_PLATFORM_H */
