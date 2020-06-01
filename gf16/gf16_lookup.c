08b3a103f07a34dae09f4c286a1db62b71958125
animetosho <animetosho@users.noreply.github.com>
2020-06-01 16:22:41 +1000

    Try to handle big endian CPUs in LH-Lookup No clue if it works as I can't test it

--- gf16/gf16_lookup.c
+++ gf16/gf16_lookup.c
@@ -1,6 +1,12 @@
 
 #include "gf16_global.h"
 
+#ifdef _MSC_VER
+# define __BYTE_ORDER__ 1234
+# define __ORDER_BIG_ENDIAN__ 4321
+#endif
+
+
 #define GF16_MULTBY_TWO_X2(p) ((((p) << 1) & 0xffffffff) ^ ((GF16_POLYNOMIAL ^ ((GF16_POLYNOMIAL&0xffff) << 16)) & -((p) >> 31)))
 #define GF16_MULTBY_TWO_X4(p) ( \
 	(((p) << 1) & 0xffffffffffffffffULL) ^ \
@@ -10,6 +16,81 @@
 	) \
 )
 
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+# define PACK_2X16(a, b) (((uint32_t)(a) << 16) | (b))
+# define PACK_4X16(a, b, c, d) (((uint64_t)(a) << 48) | ((uint64_t)(b) << 32) | ((uint64_t)(c) << 16) | (uint64_t)(d))
+#else
+# define PACK_2X16(a, b) (((uint32_t)(b) << 16) | (a))
+# define PACK_4X16(a, b, c, d) (((uint64_t)(d) << 48) | ((uint64_t)(c) << 32) | ((uint64_t)(b) << 16) | (uint64_t)(a))
+#endif
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+# define FLIP_16(n) ((((n)&0xff) << 8) | ((n) >> 8))
+# define FLIP_2X16(n) ((((n)&0xff00ff) << 8) | (((n) >> 8) & 0xff00ff))
+# define FLIP_4X16(n) ((((n)&0xff00ff00ff00ffULL) << 8) | (((n) >> 8) & 0xff00ff00ff00ffULL))
+static HEDLEY_ALWAYS_INLINE void calc_table(uint16_t coefficient, uint16_t* lhtable) {
+	int j, k;
+	
+	if(sizeof(uintptr_t) == 4) {
+		uint32_t* lhtable32 = (uint32_t*)lhtable;
+		lhtable32[0] = FLIP_16(coefficient);
+		uint32_t coefficient2 = coefficient | (coefficient << 16);
+		coefficient2 = GF16_MULTBY_TWO_X2(coefficient2);
+		uint32_t coeffFlip = FLIP_2X16(coefficient2);
+		for (j = 1; j < 128; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable32[k+j] = (coeffFlip ^ lhtable32[k]);
+			coefficient2 = GF16_MULTBY_TWO_X2(coefficient2);
+			coeffFlip = FLIP_2X16(coefficient2);
+		}
+		lhtable32[128] = FLIP_16(coefficient2 & 0xffff);
+		coefficient2 = GF16_MULTBY_TWO_X2(coefficient2);
+		coeffFlip = FLIP_2X16(coefficient2);
+		for (j = 1; j < 128; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable32[128 + k+j] = (coeffFlip ^ lhtable32[128 + k]);
+			coefficient2 = GF16_MULTBY_TWO_X2(coefficient2);
+			coeffFlip = FLIP_2X16(coefficient2);
+		}
+	} else if(sizeof(uintptr_t) >= 8) {
+		uint64_t* lhtable64 = (uint64_t*)lhtable;
+		uint32_t coefficient2 = (coefficient << 16) | coefficient; // [*1, *1]
+		coefficient2 = GF16_MULTBY_TWO_X2(coefficient2);           // [*2, *2]
+		lhtable64[0] = FLIP_4X16(((uint64_t)coefficient << 32) | ((uint64_t)(coefficient2^coefficient))); // [*0, *1, *2, *3]
+		uint64_t coefficient4 = coefficient2 | ((uint64_t)coefficient2 << 32); // [*2, *2, *2, *2]
+		coefficient4 = GF16_MULTBY_TWO_X4(coefficient4);          // [*4, *4, *4, *4]
+		uint64_t coeffFlip = FLIP_4X16(coefficient4);
+		for (j = 1; j < 64; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable64[k+j] = (coeffFlip ^ lhtable64[k]);
+			coefficient4 = GF16_MULTBY_TWO_X4(coefficient4);
+			coeffFlip = FLIP_4X16(coefficient4);
+		}
+		uint64_t tmp = coefficient4 & 0xffff0000ffffULL;      // [*0, *256, *0, *256]
+		coefficient4 = GF16_MULTBY_TWO_X4(coefficient4);      // [*512, *512, *512, *512]
+		lhtable64[64] = FLIP_4X16(tmp ^ (coefficient4 & 0xffffffff)); // [*0, *256, *512, *768]
+		coefficient4 = GF16_MULTBY_TWO_X4(coefficient4);      // [*1024, *1024, *1024, *1024]
+		coeffFlip = FLIP_4X16(coefficient4);
+		for (j = 1; j < 64; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable64[64 + k+j] = (coeffFlip ^ lhtable64[64 + k]);
+			coefficient4 = GF16_MULTBY_TWO_X4(coefficient4);
+			coeffFlip = FLIP_4X16(coefficient4);
+		}
+	} else {
+		uint16_t coeffFlip = FLIP_16(coefficient);
+		lhtable[0] = 0;
+		for (j = 1; j < 256; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable[k+j] = (coeffFlip ^ lhtable[k]);
+			coefficient = GF16_MULTBY_TWO(coefficient);
+			coeffFlip = FLIP_16(coefficient);
+		}
+		lhtable[256] = 0;
+		for (j = 1; j < 256; j <<= 1) {
+			for (k = 0; k < j; k++) lhtable[256 + k+j] = (coeffFlip ^ lhtable[256 + k]);
+			coefficient = GF16_MULTBY_TWO(coefficient);
+			coeffFlip = FLIP_16(coefficient);
+		}
+	}
+}
+
+#else /* little endian CPU */
 
 static HEDLEY_ALWAYS_INLINE void calc_table(uint16_t coefficient, uint16_t* lhtable) {
 	int j, k;
@@ -64,6 +145,7 @@ static HEDLEY_ALWAYS_INLINE void calc_table(uint16_t coefficient, uint16_t* lhta
 	}
 }
 
+#endif
 
 void gf16_lookup_mul(const void *HEDLEY_RESTRICT scratch, void *HEDLEY_RESTRICT dst, const void *HEDLEY_RESTRICT src, size_t len, uint16_t coefficient, void *HEDLEY_RESTRICT mutScratch) {
 	UNUSED(scratch); UNUSED(mutScratch);
@@ -75,16 +157,20 @@ void gf16_lookup_mul(const void *HEDLEY_RESTRICT scratch, void *HEDLEY_RESTRICT 
 	
 	if(sizeof(uintptr_t) == 4) { // assume 32-bit CPU
 		for(long ptr = -(long)len; ptr; ptr+=4) {
-			*(uint32_t*)(_dst + ptr) = lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]]
-			                         ^ ((uint32_t)(lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]) << 16);
+			*(uint32_t*)(_dst + ptr) = PACK_2X16(
+				lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]],
+				lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]
+			);
 		}
 	}
 	else if(sizeof(uintptr_t) >= 8) { // process in 64-bit
 		for(long ptr = -(long)len; ptr; ptr+=8) {
-			*(uint64_t*)(_dst + ptr) = lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]]
-			                         ^ ((uint64_t)(lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]) << 16)
-			                         ^ ((uint64_t)(lhtable[_src[ptr + 4]] ^ lhtable[256 + _src[ptr + 5]]) << 32)
-			                         ^ ((uint64_t)(lhtable[_src[ptr + 6]] ^ lhtable[256 + _src[ptr + 7]]) << 48);
+			*(uint64_t*)(_dst + ptr) = PACK_4X16(
+				lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]],
+				lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]],
+				lhtable[_src[ptr + 4]] ^ lhtable[256 + _src[ptr + 5]],
+				lhtable[_src[ptr + 6]] ^ lhtable[256 + _src[ptr + 7]]
+			);
 		}
 	}
 	else { // use 2 byte wordsize
@@ -104,16 +190,20 @@ void gf16_lookup_mul_add(const void *HEDLEY_RESTRICT scratch, void *HEDLEY_RESTR
 	
 	if(sizeof(uintptr_t) == 4) { // assume 32-bit CPU
 		for(long ptr = -(long)len; ptr; ptr+=4) {
-			*(uint32_t*)(_dst + ptr) ^= lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]]
-			                          ^ ((uint32_t)(lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]) << 16);
+			*(uint32_t*)(_dst + ptr) ^= PACK_2X16(
+				lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]],
+				lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]
+			);
 		}
 	}
 	else if(sizeof(uintptr_t) >= 8) { // process in 64-bit
 		for(long ptr = -(long)len; ptr; ptr+=8) {
-			*(uint64_t*)(_dst + ptr) ^= lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]]
-			                          ^ ((uint64_t)(lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]]) << 16)
-			                          ^ ((uint64_t)(lhtable[_src[ptr + 4]] ^ lhtable[256 + _src[ptr + 5]]) << 32)
-			                          ^ ((uint64_t)(lhtable[_src[ptr + 6]] ^ lhtable[256 + _src[ptr + 7]]) << 48);
+			*(uint64_t*)(_dst + ptr) ^= PACK_4X16(
+				lhtable[_src[ptr]] ^ lhtable[256 + _src[ptr + 1]],
+				lhtable[_src[ptr + 2]] ^ lhtable[256 + _src[ptr + 3]],
+				lhtable[_src[ptr + 4]] ^ lhtable[256 + _src[ptr + 5]],
+				lhtable[_src[ptr + 6]] ^ lhtable[256 + _src[ptr + 7]]
+			);
 		}
 	}
 	else { // use 2 byte wordsize
