9bbe643adb6a875910623f154bb0f075a47589a5
animetosho <animetosho@users.noreply.github.com>
2020-10-31 18:00:05 +1000

    MD5x2-SSE: use 64-bit shift to simulate 32-bit rotate This reduces the number of instructions needed

--- gf16/md5x2-sse.h
+++ gf16/md5x2-sse.h
@@ -1,21 +1,17 @@
 
 #define ADD _mm_add_epi32
-#define VAL(k) _mm_cvtsi64_si128(((uint64_t)k << 32) | k)
-//#define VAL(k) _mm_set1_epi32
+#define VAL _mm_set1_epi32
 #define word_t __m128i
 #define INPUT(k, set, ptr, offs, idx, var) ADD(var, VAL(k))
-#define LOAD(k, set, ptr, offs, idx, var) ADD(var = _mm_unpacklo_epi32( \
+#define LOAD(k, set, ptr, offs, idx, var) ADD(var = _mm_unpacklo_epi64( \
 	_mm_cvtsi32_si128(((uint32_t*)(ptr[0]))[idx]), \
 	_mm_cvtsi32_si128(((uint32_t*)(ptr[1]))[idx]) \
 ), VAL(k))
 
 #ifdef __SSE2__
 #include <emmintrin.h>
-// TODO: is there a way to use a 64-bit shift (by duplicating lanes) to emulate a 32-bit rotate?  probably useful, but only for SSE to avoid movdqa
-#define ROTATE(a, r) (r == 16 ? \
-	_mm_shufflelo_epi16(a, 0xb1) \
-	: _mm_or_si128(_mm_slli_epi32(a, r), _mm_srli_epi32(a, 32-r)) \
-)
+// simulate a 32b rotate by duplicating lanes and doing a single 64b shift
+#define ROTATE(a, r) _mm_srli_epi64(_mm_shuffle_epi32(a, _MM_SHUFFLE(2,2,0,0)), 32-r)
 #define _FN(f) f##_sse
 
 #define F(b,c,d) _mm_xor_si128(_mm_and_si128(_mm_xor_si128(c, d), b), d)
@@ -29,27 +25,27 @@
 #undef _FN
 
 static HEDLEY_ALWAYS_INLINE void md5_extract_x2_sse(void* dst, void* state, const int idx) {
-	__m128i* state_ = (__m128i*)state;
-	// re-arrange into two hashes
-	__m128i tmp1 = _mm_unpacklo_epi32(state_[0], state_[1]);
-	__m128i tmp2 = _mm_unpacklo_epi32(state_[2], state_[3]);
+	__m128* state_ = (__m128*)state;
+	// re-arrange to AABB, CCDD
+	__m128 tmp1 = _mm_shuffle_ps(state_[0], state_[1], _MM_SHUFFLE(2,0,2,0));
+	__m128 tmp2 = _mm_shuffle_ps(state_[2], state_[3], _MM_SHUFFLE(2,0,2,0));
 	
 	if(idx == 0) {
-		_mm_storeu_si128((__m128i*)dst, _mm_unpacklo_epi64(tmp1, tmp2));
+		_mm_storeu_ps((float*)dst, _mm_shuffle_ps(tmp1, tmp2, _MM_SHUFFLE(2,0,2,0)));
 	} else {
-		_mm_storeu_si128((__m128i*)dst, _mm_unpackhi_epi64(tmp1, tmp2));
+		_mm_storeu_ps((float*)dst, _mm_shuffle_ps(tmp1, tmp2, _MM_SHUFFLE(3,1,3,1)));
 	}
 }
 #define md5_init_lane_x2_sse(state, idx) { \
 	__m128i* state_ = (__m128i*)state; \
-	state_[0] = _mm_insert_epi16(state_[0], 0x2301, idx*2); \
-	state_[0] = _mm_insert_epi16(state_[0], 0x6745, idx*2 + 1); \
-	state_[1] = _mm_insert_epi16(state_[1], 0xab89, idx*2); \
-	state_[1] = _mm_insert_epi16(state_[1], 0xefcd, idx*2 + 1); \
-	state_[2] = _mm_insert_epi16(state_[2], 0xdcfe, idx*2); \
-	state_[2] = _mm_insert_epi16(state_[2], 0x98ba, idx*2 + 1); \
-	state_[3] = _mm_insert_epi16(state_[3], 0x5476, idx*2); \
-	state_[3] = _mm_insert_epi16(state_[3], 0x1032, idx*2 + 1); \
+	state_[0] = _mm_insert_epi16(state_[0], 0x2301, idx*4); \
+	state_[0] = _mm_insert_epi16(state_[0], 0x6745, idx*4 + 1); \
+	state_[1] = _mm_insert_epi16(state_[1], 0xab89, idx*4); \
+	state_[1] = _mm_insert_epi16(state_[1], 0xefcd, idx*4 + 1); \
+	state_[2] = _mm_insert_epi16(state_[2], 0xdcfe, idx*4); \
+	state_[2] = _mm_insert_epi16(state_[2], 0x98ba, idx*4 + 1); \
+	state_[3] = _mm_insert_epi16(state_[3], 0x5476, idx*4); \
+	state_[3] = _mm_insert_epi16(state_[3], 0x1032, idx*4 + 1); \
 }
 #endif
 
@@ -58,7 +54,7 @@ static HEDLEY_ALWAYS_INLINE void md5_extract_x2_sse(void* dst, void* state, cons
 # undef LOAD
 # define LOAD(k, set, ptr, offs, idx, var) ADD(var = _mm_insert_epi32( \
 	_mm_cvtsi32_si128(((uint32_t*)(ptr[0]))[idx]), \
-	((uint32_t*)(ptr[1]))[idx], 1 \
+	((uint32_t*)(ptr[1]))[idx], 2 \
 ), VAL(k))
 # define _FN(f) f##_avx
 # include "md5x2-base.h"
@@ -66,10 +62,10 @@ static HEDLEY_ALWAYS_INLINE void md5_extract_x2_sse(void* dst, void* state, cons
 # define md5_extract_x2_avx md5_extract_x2_sse
 #define md5_init_lane_x2_avx(state, idx) { \
 	__m128i* state_ = (__m128i*)state; \
-	state_[0] = _mm_insert_epi32(state_[0], 0x67452301L, idx); \
-	state_[1] = _mm_insert_epi32(state_[1], 0xefcdab89L, idx); \
-	state_[2] = _mm_insert_epi32(state_[2], 0x98badcfeL, idx); \
-	state_[3] = _mm_insert_epi32(state_[3], 0x10325476L, idx); \
+	state_[0] = _mm_insert_epi32(state_[0], 0x67452301L, idx*2); \
+	state_[1] = _mm_insert_epi32(state_[1], 0xefcdab89L, idx*2); \
+	state_[2] = _mm_insert_epi32(state_[2], 0x98badcfeL, idx*2); \
+	state_[3] = _mm_insert_epi32(state_[3], 0x10325476L, idx*2); \
 }
 #endif
 #ifdef ROTATE
