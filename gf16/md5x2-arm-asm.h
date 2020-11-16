82a94f3575a854f7183974f384d4fe846d6f7de4
animetosho <animetosho@users.noreply.github.com>
2020-11-16 18:51:13 +1000

    Add ARM+NEON MD5x2 Unsure if will ultimately use it

---
+++ gf16/md5x2-arm-asm.h
@@ -0,0 +1,217 @@
+#include "platform.h"
+
+#ifndef STR
+# define STR_HELPER(x) #x
+# define STR(x) STR_HELPER(x)
+#endif
+
+#ifndef UNUSED
+# define UNUSED(...) (void)(__VA_ARGS__)
+#endif
+
+
+static HEDLEY_ALWAYS_INLINE void md5_process_block_x2_scalar(uint32_t* state, const char* const* HEDLEY_RESTRICT data, size_t offset) {
+	UNUSED(offset);
+	uint32_t A1 = state[0];
+	uint32_t B1 = state[1];
+	uint32_t C1 = state[2];
+	uint32_t D1 = state[3];
+	uint32_t A2 = state[4];
+	uint32_t B2 = state[5];
+	uint32_t C2 = state[6];
+	uint32_t D2 = state[7];
+	
+	
+#ifdef __aarch64__
+# define TMP1 "w14"
+# define TMP2 "w15"
+# define SETI_L(x) "mov " TMP2 ", #" STR(x) "\n"
+# define SETI_H(x) "movk " TMP2 ", #" STR(x) ", lsl #16\n"
+# define REG(x) "%w[" STR(x) "]"
+# define ROR_ADD(A1, B1, A2, B2, R) \
+	"ror %w[" STR(A1) "], %w[" STR(A1) "], #" STR(R) "\n" \
+	"ror %w[" STR(A2) "], %w[" STR(A2) "], #" STR(R) "\n" \
+	"add %w[" STR(A1) "], %w[" STR(A1) "], %w[" STR(B1) "]\n" \
+	"add %w[" STR(A2) "], %w[" STR(A2) "], %w[" STR(B2) "]\n"
+# define ORN(dst, src1, src2) "orn " dst ", " src1 ", " src2 "\n"
+#else
+# define TMP1 "r14"
+# define TMP2 "r12"
+
+# if defined(__armv7__) || defined(_M_ARM) || defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_8A__) || (defined(__ARM_ARCH) && __ARM_ARCH >= 7)
+#  define SETI_L(x) "movw " TMP2 ", #" STR(x) "\n"
+#  define SETI_H(x) "movt " TMP2 ", #" STR(x) "\n"
+# else
+#  define SETI_L(x) "mov " TMP2 ", #" STR(x) " & 0xff\n orr " TMP2 ", #" STR(x) " & 0xff00\n"
+#  define SETI_H(x) "orr " TMP2 ", #(" STR(x) " & 0xff)<<16\n orr " TMP2 ", #(" STR(x) " & 0xff00)<<16\n"
+# endif
+
+# define REG(x) "%[" STR(x) "]"
+# define ROR_ADD(A1, B1, A2, B2, R) \
+	"add %[" STR(A1) "], %[" STR(B1) "], %[" STR(A1) "], ror #" STR(R) "\n" \
+	"add %[" STR(A2) "], %[" STR(B2) "], %[" STR(A2) "], ror #" STR(R) "\n"
+# define ORN(dst, src1, src2) "mvn " dst ", " src2 "\n orr " dst ", " dst ", " src1 "\n"
+#endif
+#define ROUND_F(A1, B1, C1, D1, A2, B2, C2, D2, NEXT_IN1, NEXT_IN2, KL, KH, R) \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	SETI_L(KL) \
+	SETI_H(KH) \
+	"add " REG(A1) ", " REG(A1) ", " TMP2 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"eor " TMP1 ", " REG(C1) ", " REG(D1) "\n" \
+	"eor " TMP2 ", " REG(C2) ", " REG(D2) "\n" \
+	"and " TMP1 ", " TMP1 ", " REG(B1) "\n" \
+	"and " TMP2 ", " TMP2 ", " REG(B2) "\n" \
+	"eor " TMP1 ", " TMP1 ", " REG(D1) "\n" \
+	"eor " TMP2 ", " TMP2 ", " REG(D2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"ldr " TMP1 ", " NEXT_IN1 "\n" \
+	"ldr " TMP2 ", " NEXT_IN2 "\n" \
+	ROR_ADD(A1, B1, A2, B2, R)
+#define ROUND_H(A1, B1, C1, D1, A2, B2, C2, D2, NEXT_IN1, NEXT_IN2, KL, KH, R) \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	SETI_L(KL) \
+	SETI_H(KH) \
+	"add " REG(A1) ", " REG(A1) ", " TMP2 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"eor " TMP1 ", " REG(C1) ", " REG(D1) "\n" \
+	"eor " TMP2 ", " REG(C2) ", " REG(D2) "\n" \
+	"eor " TMP1 ", " TMP1 ", " REG(B1) "\n" \
+	"eor " TMP2 ", " TMP2 ", " REG(B2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"ldr " TMP1 ", " NEXT_IN1 "\n" \
+	"ldr " TMP2 ", " NEXT_IN2 "\n" \
+	ROR_ADD(A1, B1, A2, B2, R)
+#define ROUND_I(A1, B1, C1, D1, A2, B2, C2, D2, NEXT_IN1, NEXT_IN2, KL, KH, R) \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	SETI_L(KL) \
+	SETI_H(KH) \
+	"add " REG(A1) ", " REG(A1) ", " TMP2 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	ORN(TMP1, REG(B1), REG(D1)) \
+	ORN(TMP2, REG(B2), REG(D2)) \
+	"eor " TMP1 ", " TMP1 ", " REG(C1) "\n" \
+	"eor " TMP2 ", " TMP2 ", " REG(C2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"ldr " TMP1 ", " NEXT_IN1 "\n" \
+	"ldr " TMP2 ", " NEXT_IN2 "\n" \
+	ROR_ADD(A1, B1, A2, B2, R)
+#define ROUND_I_LAST(A1, B1, C1, D1, A2, B2, C2, D2, KL, KH, R) \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	SETI_L(KL) \
+	SETI_H(KH) \
+	"add " REG(A1) ", " REG(A1) ", " TMP2 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	ORN(TMP1, REG(B1), REG(D1)) \
+	ORN(TMP2, REG(B2), REG(D2)) \
+	"eor " TMP1 ", " TMP1 ", " REG(C1) "\n" \
+	"eor " TMP2 ", " TMP2 ", " REG(C2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	ROR_ADD(A1, B1, A2, B2, R)
+
+#define ROUND_G(A1, B1, C1, D1, A2, B2, C2, D2, NEXT_IN1, NEXT_IN2, KL, KH, R) \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	SETI_L(KL) \
+	SETI_H(KH) \
+	"add " REG(A1) ", " REG(A1) ", " TMP2 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"bic " TMP1 ", " REG(C1) ", " REG(D1) "\n" \
+	"bic " TMP2 ", " REG(C2) ", " REG(D2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"and " TMP1 ", " REG(B1) ", " REG(D1) "\n" \
+	"and " TMP2 ", " REG(B2) ", " REG(D2) "\n" \
+	"add " REG(A1) ", " REG(A1) ", " TMP1 "\n" \
+	"add " REG(A2) ", " REG(A2) ", " TMP2 "\n" \
+	"ldr " TMP1 ", " NEXT_IN1 "\n" \
+	"ldr " TMP2 ", " NEXT_IN2 "\n" \
+	ROR_ADD(A1, B1, A2, B2, R)
+
+#define RF4(i0, i1, i2, i3, k0l, k0h, k1l, k1h, k2l, k2h, k3l, k3h) \
+	ROUND_F(A1, B1, C1, D1, A2, B2, C2, D2, "[%[i0], #" STR(i0) "]", "[%[i1], #" STR(i0) "]", k0l, k0h, 25) \
+	ROUND_F(D1, A1, B1, C1, D2, A2, B2, C2, "[%[i0], #" STR(i1) "]", "[%[i1], #" STR(i1) "]", k1l, k1h, 20) \
+	ROUND_F(C1, D1, A1, B1, C2, D2, A2, B2, "[%[i0], #" STR(i2) "]", "[%[i1], #" STR(i2) "]", k2l, k2h, 15) \
+	ROUND_F(B1, C1, D1, A1, B2, C2, D2, A2, "[%[i0], #" STR(i3) "]", "[%[i1], #" STR(i3) "]", k3l, k3h, 10)
+	
+#define RG4(i0, i1, i2, i3, k0l, k0h, k1l, k1h, k2l, k2h, k3l, k3h) \
+	ROUND_G(A1, B1, C1, D1, A2, B2, C2, D2, "[%[i0], #" STR(i0) "]", "[%[i1], #" STR(i0) "]", k0l, k0h, 27) \
+	ROUND_G(D1, A1, B1, C1, D2, A2, B2, C2, "[%[i0], #" STR(i1) "]", "[%[i1], #" STR(i1) "]", k1l, k1h, 23) \
+	ROUND_G(C1, D1, A1, B1, C2, D2, A2, B2, "[%[i0], #" STR(i2) "]", "[%[i1], #" STR(i2) "]", k2l, k2h, 18) \
+	ROUND_G(B1, C1, D1, A1, B2, C2, D2, A2, "[%[i0], #" STR(i3) "]", "[%[i1], #" STR(i3) "]", k3l, k3h, 12)
+	
+#define RH4(i0, i1, i2, i3, k0l, k0h, k1l, k1h, k2l, k2h, k3l, k3h) \
+	ROUND_H(A1, B1, C1, D1, A2, B2, C2, D2, "[%[i0], #" STR(i0) "]", "[%[i1], #" STR(i0) "]", k0l, k0h, 28) \
+	ROUND_H(D1, A1, B1, C1, D2, A2, B2, C2, "[%[i0], #" STR(i1) "]", "[%[i1], #" STR(i1) "]", k1l, k1h, 21) \
+	ROUND_H(C1, D1, A1, B1, C2, D2, A2, B2, "[%[i0], #" STR(i2) "]", "[%[i1], #" STR(i2) "]", k2l, k2h, 16) \
+	ROUND_H(B1, C1, D1, A1, B2, C2, D2, A2, "[%[i0], #" STR(i3) "]", "[%[i1], #" STR(i3) "]", k3l, k3h, 9)
+	
+#define RI4(i0, i1, i2, i3, k0l, k0h, k1l, k1h, k2l, k2h, k3l, k3h) \
+	ROUND_I(A1, B1, C1, D1, A2, B2, C2, D2, "[%[i0], #" STR(i0) "]", "[%[i1], #" STR(i0) "]", k0l, k0h, 26) \
+	ROUND_I(D1, A1, B1, C1, D2, A2, B2, C2, "[%[i0], #" STR(i1) "]", "[%[i1], #" STR(i1) "]", k1l, k1h, 22) \
+	ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "[%[i0], #" STR(i2) "]", "[%[i1], #" STR(i2) "]", k2l, k2h, 17) \
+	ROUND_I(B1, C1, D1, A1, B2, C2, D2, A2, "[%[i0], #" STR(i3) "]", "[%[i1], #" STR(i3) "]", k3l, k3h, 11)
+	
+	asm(
+		"ldr " TMP1 ", [%[i0]]\n"
+		"ldr " TMP2 ", [%[i1]]\n"
+		RF4( 4,  8, 12, 16,  0xa478, 0xd76a, 0xb756, 0xe8c7, 0x70db, 0x2420, 0xceee, 0xc1bd)
+		RF4(20, 24, 28, 32,  0x0faf, 0xf57c, 0xc62a, 0x4787, 0x4613, 0xa830, 0x9501, 0xfd46)
+		RF4(36, 40, 44, 48,  0x98d8, 0x6980, 0xf7af, 0x8b44, 0x5bb1, 0xffff, 0xd7be, 0x895c)
+		RF4(52, 56, 60,  4,  0x1122, 0x6b90, 0x7193, 0xfd98, 0x438e, 0xa679, 0x0821, 0x49b4)
+		
+		RG4(24, 44,  0, 20,  0x2562, 0xf61e, 0xb340, 0xc040, 0x5a51, 0x265e, 0xc7aa, 0xe9b6)
+		RG4(40, 60, 16, 36,  0x105d, 0xd62f, 0x1453, 0x0244, 0xe681, 0xd8a1, 0xfbc8, 0xe7d3)
+		RG4(56, 12, 32, 52,  0xcde6, 0x21e1, 0x07d6, 0xc337, 0x0d87, 0xf4d5, 0x14ed, 0x455a)
+		RG4( 8, 28, 48, 20,  0xe905, 0xa9e3, 0xa3f8, 0xfcef, 0x02d9, 0x676f, 0x4c8a, 0x8d2a)
+		
+		RH4(32, 44, 56,  4,  0x3942, 0xfffa, 0xf681, 0x8771, 0x6122, 0x6d9d, 0x380c, 0xfde5)
+		RH4(16, 28, 40, 52,  0xea44, 0xa4be, 0xcfa9, 0x4bde, 0x4b60, 0xf6bb, 0xbc70, 0xbebf)
+		RH4( 0, 12, 24, 36,  0x7ec6, 0x289b, 0x27fa, 0xeaa1, 0x3085, 0xd4ef, 0x1d05, 0x0488)
+		RH4(48, 60,  8,  0,  0xd039, 0xd9d4, 0x99e5, 0xe6db, 0x7cf8, 0x1fa2, 0x5665, 0xc4ac)
+		
+		RI4(28, 56, 20, 48,  0x2244, 0xf429, 0xff97, 0x432a, 0x23a7, 0xab94, 0xa039, 0xfc93)
+		RI4(12, 40,  4, 32,  0x59c3, 0x655b, 0xcc92, 0x8f0c, 0xf47d, 0xffef, 0x5dd1, 0x8584)
+		RI4(60, 24, 52, 16,  0x7e4f, 0x6fa8, 0xe6e0, 0xfe2c, 0x4314, 0xa301, 0x11a1, 0x4e08)
+		
+		ROUND_I(A1, B1, C1, D1, A2, B2, C2, D2, "[%[i0], #44]", "[%[i1], #44]", 0x7e82, 0xf753, 26)
+		ROUND_I(D1, A1, B1, C1, D2, A2, B2, C2, "[%[i0], #8]", "[%[i1], #8]", 0xf235, 0xbd3a, 22)
+		ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "[%[i0], #36]", "[%[i1], #36]", 0xd2bb, 0x2ad7, 17)
+		ROUND_I_LAST(B1, C1, D1, A1, B2, C2, D2, A2, 0xd391, 0xeb86, 11)
+	: [A1]"+r"(A1), [B1]"+r"(B1), [C1]"+r"(C1), [D1]"+r"(D1),
+	  [A2]"+r"(A2), [B2]"+r"(B2), [C2]"+r"(C2), [D2]"+r"(D2)
+	: [i0]"r"(data[0]), [i1]"r"(data[1])
+	: TMP1, TMP2
+	);
+	state[0] += A1;
+	state[1] += B1;
+	state[2] += C1;
+	state[3] += D1;
+	state[4] += A2;
+	state[5] += B2;
+	state[6] += C2;
+	state[7] += D2;
+#undef ROUND_F
+#undef ROUND_G
+#undef ROUND_H
+#undef ROUND_I
+#undef RF4
+#undef RG4
+#undef RH4
+#undef RI4
+
+#undef TMP1
+#undef TMP2
+#undef SETI_L
+#undef SETI_H
+#undef ROR_ADD
+#undef ORN
+}
---
+++ gf16/md5x2-neon-asm.h
@@ -0,0 +1,283 @@
+#include "platform.h"
+
+#ifndef STR
+# define STR_HELPER(x) #x
+# define STR(x) STR_HELPER(x)
+#endif
+
+ALIGN_TO(16, static const uint32_t md5_constants[64]) = {
+	// F
+	0xd76aa478L, 0xe8c7b756L, 0x242070dbL, 0xc1bdceeeL, 0xf57c0fafL, 0x4787c62aL, 0xa8304613L, 0xfd469501L,
+	0x698098d8L, 0x8b44f7afL, 0xffff5bb1L, 0x895cd7beL, 0x6b901122L, 0xfd987193L, 0xa679438eL, 0x49b40821L,
+	
+	// G (sequenced: 3,0,1,2)
+	0xe9b6c7aaL, 0xf61e2562L, 0xc040b340L, 0x265e5a51L,
+	0xe7d3fbc8L, 0xd62f105dL, 0x02441453L, 0xd8a1e681L,
+	0x455a14edL, 0x21e1cde6L, 0xc33707d6L, 0xf4d50d87L,
+	0x8d2a4c8aL, 0xa9e3e905L, 0xfcefa3f8L, 0x676f02d9L,
+	
+	// H
+	0xfffa3942L, 0x8771f681L, 0x6d9d6122L, 0xfde5380cL,
+	0xa4beea44L, 0x4bdecfa9L, 0xf6bb4b60L, 0xbebfbc70L,
+	0x289b7ec6L, 0xeaa127faL, 0xd4ef3085L, 0x04881d05L,
+	0xd9d4d039L, 0xe6db99e5L, 0x1fa27cf8L, 0xc4ac5665L,
+	
+	// I
+	0xf4292244L, 0x432aff97L, 0xab9423a7L, 0xfc93a039L,
+	0x655b59c3L, 0x8f0ccc92L, 0xffeff47dL, 0x85845dd1L,
+	0x6fa87e4fL, 0xfe2ce6e0L, 0xa3014314L, 0x4e0811a1L,
+	0xf7537e82L, 0xbd3af235L, 0x2ad7d2bbL, 0xeb86d391L
+};
+
+
+
+#ifdef __ARM_NEON
+static HEDLEY_ALWAYS_INLINE void md5_process_block_x2_neon(uint32x2_t* state, const char* const* HEDLEY_RESTRICT data, size_t offset) {
+	UNUSED(offset);
+	uint32x2_t A = state[0];
+	uint32x2_t B = state[1];
+	uint32x2_t C = state[2];
+	uint32x2_t D = state[3];
+	
+	const uint32_t* k = md5_constants;
+	
+#ifdef __aarch64__
+#define ROUND_X(A, B, I, R, L) \
+	"add %[" STR(A) "].2s, %[" STR(A) "].2s, " I ".2s\n" \
+	"add v20.2s, %[" STR(A) "].2s, v20.2s\n" \
+	"ushr %[" STR(A) "].2s, v20.2s, #" STR(R) "\n" \
+	"sli %[" STR(A) "].2s, v20.2s, #" STR(L) "\n" \
+	"add %[" STR(A) "].2s, %[" STR(A) "].2s, %[" STR(B) "].2s\n" \
+
+#define ROUND_F(A, B, C, D, I, R, L) \
+	"mov v20.8b, %[" STR(D) "].8b\n" \
+	"bit v20.8b, %[" STR(C) "].8b, %[" STR(B) "].8b\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_G(A, B, C, D, I, R, L) \
+	"mov v20.8b, %[" STR(D) "].8b\n" \
+	"bsl v20.8b, %[" STR(B) "].8b, %[" STR(C) "].8b\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_H(A, B, C, D, I, R, L) \
+	"eor v20.8b, %[" STR(C) "].8b, %[" STR(D) "].8b\n" \
+	"eor v20.8b, v20.8b, %[" STR(B) "].8b\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_H16(A, B, C, D, I) \
+	"eor v20.8b, %[" STR(C) "].8b, %[" STR(D) "].8b\n" \
+	"eor v20.8b, v20.8b, %[" STR(B) "].8b\n" \
+	"add %[" STR(A) "].2s, %[" STR(A) "].2s, " I ".2s\n" \
+	"add %[" STR(A) "].2s, %[" STR(A) "].2s, v20.2s\n" \
+	"rev32 %[" STR(A) "].4h, %[" STR(A) "].4h\n" \
+	"add %[" STR(A) "].2s, %[" STR(A) "].2s, %[" STR(B) "].2s\n"
+#define ROUND_I(A, B, C, D, I, R, L) \
+	"orn v20.8b, %[" STR(B) "].8b, %[" STR(D) "].8b\n" \
+	"eor v20.8b, v20.8b, %[" STR(C) "].8b\n" \
+	ROUND_X(A, B, I, R, L)
+
+#define RF4(r1, r2) \
+	"ld4r {v16.2s, v17.2s, v18.2s, v19.2s}, [%[k]], #16\n" \
+	"add v16.2s, v16.2s, " r1 ".2s\n" \
+	ROUND_F(A, B, C, D, "v16", 25,  7) \
+	"ext v16.16b, " r1 ".16b, " r1 ".16b, #8\n" \
+	"add v17.2s, v16.2s, v17.2s\n" \
+	ROUND_F(D, A, B, C, "v17", 20, 12) \
+	"add v18.2s, v18.2s, " r2 ".2s\n" \
+	ROUND_F(C, D, A, B, "v18", 15, 17) \
+	"ext v16.16b, " r2 ".16b, " r2 ".16b, #8\n" \
+	"add v19.2s, v16.2s, v19.2s\n" \
+	ROUND_F(B, C, D, A, "v19", 10, 22)
+
+#define RG4(rs, r1, r2) \
+	"ld4r {v16.2s, v17.2s, v18.2s, v19.2s}, [%[k]], #16\n" \
+	"zip1 v16.2d, v16.2d, v17.2d\n" \
+	"add v16.4s, v16.4s, " rs ".4s\n" \
+	"ext v17.16b, v16.16b, v16.16b, #8\n" \
+	ROUND_G(A, B, C, D, "v17", 27,  5) \
+	"add v18.2s, v18.2s, " r1 ".2s\n" \
+	"ext v17.16b, " r2 ".16b, " r2 ".16b, #8\n" \
+	ROUND_G(D, A, B, C, "v18", 23,  9) \
+	"add v19.2s, v19.2s, v17.2s\n" \
+	ROUND_G(C, D, A, B, "v19", 18, 14) \
+	ROUND_G(B, C, D, A, "v16", 12, 20)
+
+#define RH4(r1, r2, r3, r4) \
+	"ld4r {v16.2s, v17.2s, v18.2s, v19.2s}, [%[k]], #16\n" \
+	"ext v21.16b, " r1 ".16b, " r1 ".16b, #8\n" \
+	"ext v22.16b, " r3 ".16b, " r3 ".16b, #8\n" \
+	"add v16.2s, v16.2s, v21.2s\n" \
+	"add v17.2s, v17.2s, " r2 ".2s\n" \
+	"add v18.2s, v18.2s, v22.2s\n" \
+	"add v19.2s, v19.2s, " r4 ".2s\n" \
+	ROUND_H(A, B, C, D, "v16", 28,  4) \
+	ROUND_H(D, A, B, C, "v17", 21, 11) \
+	ROUND_H16(C, D, A, B, "v18") \
+	ROUND_H(B, C, D, A, "v19",  9, 23)
+
+#define RI4(r1, r2, r3, r4) \
+	"ld4r {v16.2s, v17.2s, v18.2s, v19.2s}, [%[k]], #16\n" \
+	"ext v21.16b, " r2 ".16b, " r2 ".16b, #8\n" \
+	"ext v22.16b, " r4 ".16b, " r4 ".16b, #8\n" \
+	"add v16.2s, v16.2s, " r1 ".2s\n" \
+	"add v17.2s, v17.2s, v21.2s\n" \
+	"add v18.2s, v18.2s, " r3 ".2s\n" \
+	"add v19.2s, v19.2s, v22.2s\n" \
+	ROUND_I(A, B, C, D, "v16", 26,  6) \
+	ROUND_I(D, A, B, C, "v17", 22, 10) \
+	ROUND_I(C, D, A, B, "v18", 17, 15) \
+	ROUND_I(B, C, D, A, "v19", 11, 21)
+
+	asm(
+		"ld1 {v20.16b, v21.16b, v22.16b, v23.16b}, [%[i0]]\n"
+		"ld1 {v28.16b, v29.16b, v30.16b, v31.16b}, [%[i1]]\n"
+		"zip1 v24.4s, v20.4s, v28.4s\n"
+		"zip2 v28.4s, v20.4s, v28.4s\n"
+		RF4("v24", "v28")
+		"zip1 v25.4s, v21.4s, v29.4s\n"
+		"zip2 v29.4s, v21.4s, v29.4s\n"
+		RF4("v25", "v29")
+		"zip1 v26.4s, v22.4s, v30.4s\n"
+		"zip2 v30.4s, v22.4s, v30.4s\n"
+		RF4("v26", "v30")
+		"zip1 v27.4s, v23.4s, v31.4s\n"
+		"zip2 v31.4s, v23.4s, v31.4s\n"
+		RF4("v27", "v31")
+		
+		RG4("v24", "v29", "v30")
+		RG4("v25", "v30", "v31")
+		RG4("v26", "v31", "v28")
+		RG4("v27", "v28", "v29")
+		
+		RH4("v25", "v26", "v30", "v31")
+		RH4("v24", "v25", "v29", "v30")
+		RH4("v27", "v24", "v28", "v29")
+		RH4("v26", "v27", "v31", "v28")
+		
+		RI4("v24", "v29", "v31", "v25")
+		RI4("v27", "v28", "v30", "v24")
+		RI4("v26", "v31", "v29", "v27")
+		RI4("v25", "v30", "v28", "v26")
+	: [A]"+w"(A), [B]"+w"(B), [C]"+w"(C), [D]"+w"(D), [k]"+r"(k)
+	: [i0]"r"(data[0]), [i1]"r"(data[1])
+	: "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31"
+	);
+
+#else
+#define ROUND_X(A, B, I, R, L) \
+	"vadd.i32 %P[" STR(A) "], %P[" STR(A) "], " I "\n" \
+	"vadd.i32 d11, %P[" STR(A) "], d11\n" \
+	"vshr.u32 %P[" STR(A) "], d11, #" STR(R) "\n" \
+	"vsli.32  %P[" STR(A) "], d11, #" STR(L) "\n" \
+	"vadd.i32 %P[" STR(A) "], %P[" STR(A) "], %P[" STR(B) "]\n" \
+
+#define ROUND_F(A, B, C, D, I, R, L) \
+	"vorr d11, %P[" STR(D) "], %P[" STR(D) "]\n" \
+	"vbit d11, %P[" STR(C) "], %P[" STR(B) "]\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_G(A, B, C, D, I, R, L) \
+	"vorr d11, %P[" STR(D) "], %P[" STR(D) "]\n" \
+	"vbsl d11, %P[" STR(B) "], %P[" STR(C) "]\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_H(A, B, C, D, I, R, L) \
+	"veor d11, %P[" STR(C) "], %P[" STR(D) "]\n" \
+	"veor d11, d11, %P[" STR(B) "]\n" \
+	ROUND_X(A, B, I, R, L)
+#define ROUND_H16(A, B, C, D, I) \
+	"veor d11, %P[" STR(C) "], %P[" STR(D) "]\n" \
+	"veor d11, d11, %P[" STR(B) "]\n" \
+	"vadd.i32 %P[" STR(A) "], %P[" STR(A) "], " I "\n" \
+	"vadd.i32 %P[" STR(A) "], %P[" STR(A) "], d11\n" \
+	"vrev32.i16 %P[" STR(A) "], %P[" STR(A) "]\n" \
+	"vadd.i32 %P[" STR(A) "], %P[" STR(A) "], %P[" STR(B) "]\n"
+#define ROUND_I(A, B, C, D, I, R, L) \
+	"vorn d11, %P[" STR(B) "], %P[" STR(D) "]\n" \
+	"veor d11, d11, %P[" STR(C) "]\n" \
+	ROUND_X(A, B, I, R, L)
+
+#define RF4(r1, r2) \
+	"vld4.32 {d12[],d13[],d14[],d15[]}, [%[k] :128]!\n" \
+	"vzip.32 " r1 ", " r2 "\n" \
+	"vadd.i32 q6, q6, " r1 "\n" \
+	"vadd.i32 q7, q7, " r2 "\n" \
+	ROUND_F(A, B, C, D, "d12", 25,  7) \
+	ROUND_F(D, A, B, C, "d13", 20, 12) \
+	ROUND_F(C, D, A, B, "d14", 15, 17) \
+	ROUND_F(B, C, D, A, "d15", 10, 22)
+	
+#define RG4(rs, r1, r2) \
+	"vld4.32 {d12[],d13[],d14[],d15[]}, [%[k] :128]!\n" \
+	"vadd.i32 q6, q6, " rs "\n" \
+	"vadd.i32 d14, d14, " r1 " \n" \
+	"vadd.i32 d15, d15, " r2 " \n" \
+	ROUND_G(A, B, C, D, "d13", 27,  5) \
+	ROUND_G(D, A, B, C, "d14", 23,  9) \
+	ROUND_G(C, D, A, B, "d15", 18, 14) \
+	ROUND_G(B, C, D, A, "d12", 12, 20)
+	
+#define RH4(r1, r2, r3, r4) \
+	"vld4.32 {d12[],d13[],d14[],d15[]}, [%[k] :128]!\n" \
+	"vadd.i32 d12, d12, " r1 " \n" \
+	"vadd.i32 d13, d13, " r2 " \n" \
+	"vadd.i32 d14, d14, " r3 " \n" \
+	"vadd.i32 d15, d15, " r4 " \n" \
+	ROUND_H(A, B, C, D, "d12", 28,  4) \
+	ROUND_H(D, A, B, C, "d13", 21, 11) \
+	ROUND_H16(C, D, A, B, "d14") \
+	ROUND_H(B, C, D, A, "d15",  9, 23)
+	
+#define RI4(r1, r2, r3, r4) \
+	"vld4.32 {d12[],d13[],d14[],d15[]}, [%[k] :128]!\n" \
+	"vadd.i32 d12, d12, " r1 " \n" \
+	"vadd.i32 d13, d13, " r2 " \n" \
+	"vadd.i32 d14, d14, " r3 " \n" \
+	"vadd.i32 d15, d15, " r4 " \n" \
+	ROUND_I(A, B, C, D, "d12", 26,  6) \
+	ROUND_I(D, A, B, C, "d13", 22, 10) \
+	ROUND_I(C, D, A, B, "d14", 17, 15) \
+	ROUND_I(B, C, D, A, "d15", 11, 21)
+	
+	asm(
+		"vld1.32 {d16-d19}, [%[i0]]\n"
+		"add r4, %[i0], #32\n"
+		"vld1.32 {d24-d27}, [%[i1]]\n"
+		"add r5, %[i1], #32\n"
+		"vld1.32 {d20-d23}, [r4]\n"
+		"vld1.32 {d28-d31}, [r5]\n"
+		RF4("q8", "q12")
+		RF4("q9", "q13")
+		RF4("q10", "q14")
+		RF4("q11", "q15")
+		
+		RG4("q8", "d26", "d29")
+		RG4("q9", "d28", "d31")
+		RG4("q10", "d30", "d25")
+		RG4("q11", "d24", "d27")
+		
+		RH4("d19", "d20", "d29", "d30")
+		RH4("d17", "d18", "d27", "d28")
+		RH4("d23", "d16", "d25", "d26")
+		RH4("d21", "d22", "d31", "d24")
+		
+		RI4("d16", "d27", "d30", "d19")
+		RI4("d22", "d25", "d28", "d17")
+		RI4("d20", "d31", "d26", "d23")
+		RI4("d18", "d29", "d24", "d21")
+	: [A]"+w"(A), [B]"+w"(B), [C]"+w"(C), [D]"+w"(D), [k]"+r"(k)
+	: [i0]"r"(data[0]), [i1]"r"(data[1])
+	: "d11", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "r4", "r5"
+	);
+#endif
+	
+	state[0] = vadd_u32(A, state[0]);
+	state[1] = vadd_u32(B, state[1]);
+	state[2] = vadd_u32(C, state[2]);
+	state[3] = vadd_u32(D, state[3]);
+#undef ROUND_X
+#undef ROUND_F
+#undef ROUND_G
+#undef ROUND_H
+#undef ROUND_I
+#undef RF4
+#undef RG4
+#undef RH4
+#undef RI4
+}
+#endif
