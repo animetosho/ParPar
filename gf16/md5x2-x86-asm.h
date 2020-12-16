277f0371108973a56c17982181482d197b42c359
animetosho <animetosho@users.noreply.github.com>
2020-12-16 13:54:45 +1000

    MD5x2 x86: fix compilation for GCC's failure to compile For whatever odd reason, GCC may insist on forcing addresses into a register, barfing up its own register allocation, instead of directly using them This divvies up allocation, allowing it to go on its craziness (and considerably slowing things down)

--- gf16/md5x2-x86-asm.h
+++ gf16/md5x2-x86-asm.h
@@ -120,44 +120,49 @@ static HEDLEY_ALWAYS_INLINE void md5_process_block_x2_scalar(uint32_t* state, co
 	"addl %k[" STR(B1) "], %k[" STR(A1) "]\n" \
 	"addl %k[" STR(B2) "], %k[" STR(A2) "]\n"
 
-#define ASM_PARAMS(i0, i1, i2, i3) \
+#define ASM_PARAMS(i0, i1) \
 	[A1]"+r"(A1), [B1]"+r"(B1), [C1]"+r"(C1), [D1]"+r"(D1), \
 	[A2]"+r"(A2), [B2]"+r"(B2), [C2]"+r"(C2), [D2]"+r"(D2), \
 	[TMP1]"+r"(tmp1), [TMP2]"+r"(tmp2) \
-: [i0_0]"m"(_data[0][i0]), [i0_1]"m"(_data[0][i1]), [i0_2]"m"(_data[0][i2]), [i0_3]"m"(_data[0][i3]), \
-  [i1_0]"m"(_data[1][i0]), [i1_1]"m"(_data[1][i1]), [i1_2]"m"(_data[1][i2]), [i1_3]"m"(_data[1][i3]) \
+: [i0_0]"m"(_data[0][i0]), [i0_1]"m"(_data[0][i1]), \
+  [i1_0]"m"(_data[1][i0]), [i1_1]"m"(_data[1][i1]) \
 :
 
 #define RF4(i0, i1, i2, i3, k0, k1, k2, k3) asm( \
 	ROUND_F(A1, B1, C1, D1, A2, B2, C2, D2, "%[i0_0]", "%[i1_0]", k0, 7) \
 	ROUND_F(D1, A1, B1, C1, D2, A2, B2, C2, "%[i0_1]", "%[i1_1]", k1, 12) \
-	ROUND_F(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_2]", "%[i1_2]", k2, 17) \
-	ROUND_F(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_3]", "%[i1_3]", k3, 22) \
-: ASM_PARAMS(i0, i1, i2, i3));
+: ASM_PARAMS(i0, i1)); asm( \
+	ROUND_F(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_0]", "%[i1_0]", k2, 17) \
+	ROUND_F(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_1]", "%[i1_1]", k3, 22) \
+: ASM_PARAMS(i2, i3));
 	
 #define RG4(i0, i1, i2, i3, k0, k1, k2, k3) asm( \
 	ROUND_G(A1, B1, C1, D1, A2, B2, C2, D2, "%[i0_0]", "%[i1_0]", k0, 5) \
 	ROUND_G(D1, A1, B1, C1, D2, A2, B2, C2, "%[i0_1]", "%[i1_1]", k1, 9) \
-	ROUND_G(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_2]", "%[i1_2]", k2, 14) \
-	ROUND_G(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_3]", "%[i1_3]", k3, 20) \
-: ASM_PARAMS(i0, i1, i2, i3));
+: ASM_PARAMS(i0, i1)); asm( \
+	ROUND_G(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_0]", "%[i1_0]", k2, 14) \
+	ROUND_G(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_1]", "%[i1_1]", k3, 20) \
+: ASM_PARAMS(i2, i3));
 	
 #define RH4(i0, i1, i2, i3, k0, k1, k2, k3) asm( \
 	ROUND_H(A1, B1, C1, D1, A2, B2, C2, D2, "%[i0_0]", "%[i1_0]", k0, 4) \
 	ROUND_H(D1, A1, B1, C1, D2, A2, B2, C2, "%[i0_1]", "%[i1_1]", k1, 11) \
-	ROUND_H(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_2]", "%[i1_2]", k2, 16) \
-	ROUND_H(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_3]", "%[i1_3]", k3, 23) \
-: ASM_PARAMS(i0, i1, i2, i3));
+: ASM_PARAMS(i0, i1)); asm( \
+	ROUND_H(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_0]", "%[i1_0]", k2, 16) \
+	ROUND_H(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_1]", "%[i1_1]", k3, 23) \
+: ASM_PARAMS(i2, i3));
 	
 #define RI4(i0, i1, i2, i3, k0, k1, k2, k3) asm( \
 	ROUND_I(A1, B1, C1, D1, A2, B2, C2, D2, "%[i0_0]", "%[i1_0]", k0, 6) \
 	ROUND_I(D1, A1, B1, C1, D2, A2, B2, C2, "%[i0_1]", "%[i1_1]", k1, 10) \
-	ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_2]", "%[i1_2]", k2, 15) \
-	ROUND_I(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_3]", "%[i1_3]", k3, 21) \
-: ASM_PARAMS(i0, i1, i2, i3));
+: ASM_PARAMS(i0, i1)); asm( \
+	ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_0]", "%[i1_0]", k2, 15) \
+	ROUND_I(B1, C1, D1, A1, B2, C2, D2, A2, "%[i0_1]", "%[i1_1]", k3, 21) \
+: ASM_PARAMS(i2, i3));
 	
 	A1 += _data[0][0];
 	A2 += _data[1][0];
+	asm("" : "=r"(tmp1), "=r"(tmp2) ::); // silence compiler warning
 	
 	RF4( 1,  2,  3,  4,  -0x28955b88, -0x173848aa, 0x242070db, -0x3e423112)
 	RF4( 5,  6,  7,  8,  -0x0a83f051, 0x4787c62a, -0x57cfb9ed, -0x02b96aff)
@@ -181,9 +186,10 @@ static HEDLEY_ALWAYS_INLINE void md5_process_block_x2_scalar(uint32_t* state, co
 	asm(
 		ROUND_I(A1, B1, C1, D1, A2, B2, C2, D2, "%[i0_0]", "%[i1_0]", -0x08ac817e, 6)
 		ROUND_I(D1, A1, B1, C1, D2, A2, B2, C2, "%[i0_1]", "%[i1_1]", -0x42c50dcb, 10)
-		ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_2]", "%[i1_2]", 0x2ad7d2bb, 15)
+	: ASM_PARAMS(11, 2)); asm(
+		ROUND_I(C1, D1, A1, B1, C2, D2, A2, B2, "%[i0_0]", "%[i1_0]", 0x2ad7d2bb, 15)
 		ROUND_I_LAST(B1, C1, D1, A1, B2, C2, D2, A2, -0x14792c6f, 21)
-	: ASM_PARAMS(11, 2, 9, 0));
+	: ASM_PARAMS(9, 0));
 	state[0] += A1;
 	state[1] += B1;
 	state[2] += C1;
