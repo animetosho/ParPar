d1d3da11a9a1a1853de53b6fe6efa15f26f48c71
animetosho <animetosho@users.noreply.github.com>
2023-05-25 20:20:21 +1000

    Disable scalar MD5 ASM on debug builds ...because the compiler may run out of registers

--- hasher/md5-scalar.h
+++ hasher/md5-scalar.h
@@ -3,7 +3,7 @@
 #include "../src/platform.h"
 #include "../src/stdint.h"
 
-#if (defined(__GNUC__) || defined(__clang__)) && defined(PLATFORM_X86)
+#if (defined(__GNUC__) || defined(__clang__)) && defined(PLATFORM_X86) && defined(__OPTIMIZE__)
 # define MD5_USE_ASM
 # ifdef PLATFORM_AMD64
 #  define MD5_HAS_NOLEA 1
@@ -14,7 +14,7 @@
 #else
 # define md5_process_block_nolea md5_process_block_scalar
 #endif
-#if (defined(__GNUC__) || defined(__clang__)) && defined(PLATFORM_ARM)
+#if (defined(__GNUC__) || defined(__clang__)) && defined(PLATFORM_ARM) && defined(__OPTIMIZE__)
 # define MD5_USE_ASM
 # ifdef __aarch64__
 #  include "md5-arm64-asm.h"
