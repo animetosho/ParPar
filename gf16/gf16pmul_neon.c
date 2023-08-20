88b3f2086ba66704abe703883dd24e09f672e00a
animetosho <animetosho@users.noreply.github.com>
2023-08-20 10:22:22 +1000

    Fix compile on AArch32 + EOL fix

--- gf16/gf16pmul_neon.c
+++ gf16/gf16pmul_neon.c
@@ -20,14 +20,20 @@ void gf16pmul_neon(void *HEDLEY_RESTRICT dst, const void* src1, const void* src2
 	for(intptr_t ptr = -(intptr_t)len; ptr; ptr += sizeof(uint8x16_t)*2) {
 		poly8x16x2_t data1 = vld2q_p8(_src1+ptr);
 		poly8x16x2_t data2 = vld2q_p8(_src2+ptr);
-		poly16x8_t low1 = pmull_low(data1.val[0], data2.val[0]);
-		poly16x8_t low2 = pmull_high(data1.val[0], data2.val[0]);
+		poly16x8_t low1 = vmull_p8(vget_low_p8(data1.val[0]), vget_low_p8(data2.val[0]));
 		poly8x16_t dataMid1 = veorq_p8(data1.val[0], data1.val[1]);
 		poly8x16_t dataMid2 = veorq_p8(data2.val[0], data2.val[1]);
-		poly16x8_t mid1 = pmull_low(dataMid1, dataMid2);
+		poly16x8_t mid1 = vmull_p8(vget_low_p8(dataMid1), vget_low_p8(dataMid2));
+		poly16x8_t high1 = vmull_p8(vget_low_p8(data1.val[1]), vget_low_p8(data2.val[1]));
+#ifdef __aarch64__
+		poly16x8_t low2 = pmull_high(data1.val[0], data2.val[0]);
 		poly16x8_t mid2 = pmull_high(dataMid1, dataMid2);
-		poly16x8_t high1 = pmull_low(data1.val[1], data2.val[1]);
 		poly16x8_t high2 = pmull_high(data1.val[1], data2.val[1]);
+#else
+		poly16x8_t low2 = vmull_p8(vget_high_p8(data1.val[0]), vget_high_p8(data2.val[0]));
+		poly16x8_t mid2 = vmull_p8(vget_high_p8(dataMid1), vget_high_p8(dataMid2));
+		poly16x8_t high2 = vmull_p8(vget_high_p8(data1.val[1]), vget_high_p8(data2.val[1]));
+#endif
 		
 		gf16_clmul_neon_reduction(&low1, low2, mid1, mid2, &high1, high2);
 		uint8x16x2_t out;
